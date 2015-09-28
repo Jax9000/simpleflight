ca979bf5ebafe675964f302d6c550dea2824f79d
Jax9000 <jax1994@gmail.com>
2015-09-28 19:05:44 +0200

    4 color square

--- simpleflight/main.cpp
+++ simpleflight/main.cpp
@@ -36,7 +36,7 @@ void KeyEvent(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	// When a user presses the escape key, we set the WindowShouldClose property to true, 
 	// closing the application
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_ENTER) && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
@@ -90,20 +90,32 @@ GLuint CreateProgram(GLuint vertexShader, GLuint fragmentShader)
 	return shaderProgram;
 }
 
-const GLchar* vertexShaderSource = 
+const GLchar* vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"vertexColor = vec4(color, 1.0f); \n"
 "}\0";
 
-const GLchar* fragmentShaderSource =
+const GLchar* fragmentShaderSource0 =
+"#version 330 core\n"
+"in vec4 vertexColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"	color = vertexColor;\n"
+"}\0";
+
+const GLchar* fragmentShaderSource1 =
 "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
-"	color = vec4(1.0f, 1f, 0.2f, 1.0f);\n"
+"	color = vec4(0.0f, 1f, 0.2f, 1.0f);\n"
 "}\0";
 
 int main()
@@ -116,24 +128,57 @@ int main()
 		return Terminate("Failed to init glew");
 
 	GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderSource, "VERTEX");
-	GLuint fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource, "FRAGMENT");
-	GLuint shaderProgram = CreateProgram(vertexShader, fragmentShader);
+	GLuint fragmentShader0 = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource0, "FRAGMENT0");
+	GLuint fragmentShader1 = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource1, "FRAGMENT1");
+	GLuint shaderProgram0 = CreateProgram(vertexShader, fragmentShader0);
+	GLuint shaderProgram1 = CreateProgram(vertexShader, fragmentShader1);
 
 	glfwSetKeyCallback(window, KeyEvent);
 
 	GLfloat vertices[] = {
-		0.0f, 0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f
+		0.50f, 0.50f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		
+		-0.50f, -0.50f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		
+		0.50f, -0.50f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		
+		-0.50f, 0.50f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 2,   // First Triangle
+		0, 1, 3    // Second Triangle
 	};
 
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	
+	glBindVertexArray(VAO);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);				
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -143,6 +188,13 @@ int main()
 		//rendering
 		glClearColor(1, 0.85f, 0.3f, 0.8f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+
+			glUseProgram(shaderProgram0);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
