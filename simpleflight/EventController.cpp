30199324a9230093aad19eb5c8e0c58e35141f14
jax9000 <jax1994@gmail.com>
2016-02-25 00:18:39 +0100

    Add implementations to Event Controller methods

--- simpleflight/EventController.cpp
+++ simpleflight/EventController.cpp
@@ -1,7 +1,6 @@
 #include "EventController.h"
 
 
-
 EventController::EventController()
 {
 }
@@ -11,12 +10,82 @@ EventController::~EventController()
 {
 }
 
+EventController& EventController::EventController::GetInstance()
+{
+	static EventController instance;
+	return instance;
+}
+
+void EventController::AddMouseListener(IMouseListener* listener)
+{
+	mouse_listiners.push_back(listener);
+}
+
+void EventController::AddKeyListener(IKeyListener* listener)
+{
+	key_listiners.push_back(listener);
+}
+
+void EventController::RemoveMouseListener(IMouseListener* listener)
+{
+	std::remove(mouse_listiners.begin(), mouse_listiners.end(), listener);
+}
+
+void EventController::RemoveKeyListener(IKeyListener* listener)
+{
+	std::remove(key_listiners.begin(), key_listiners.end(), listener);
+}
+
 void EventController::KeyNotify(int key, int action)
 {
+	if (action == GLFW_PRESS)
+	{
+		key_press = true;
+		for (IKeyListener* listener : key_listiners)
+			listener->OnKeyPressed(key);
+	}
 
+	if (action == GLFW_RELEASE)
+	{
+		key_press = false;
+		key_hold = false;
+	}
+
+	if (key_hold)
+	{
+		for (IKeyListener* listener : key_listiners)
+			listener->OnKeyHold(key);
+	}
+
+	if (key_press)
+	{
+		key_hold = true;
+	}
 }
 
-void EventController::MouseNotify(int xpos, int ypos)
+void EventController::MouseNotify(double xpos, double ypos)
 {
+	if (first_mouse_measurment)
+	{
+		EventController::last_x = xpos;
+		EventController::last_y = ypos;
+		first_mouse_measurment = false;
+	}
 
+	double xoffset = xpos - EventController::last_x;
+	double yoffset = EventController::last_y - ypos;  // Reversed since y-coordinates go from bottom to left
+
+	last_x = xpos;
+	last_y = ypos;
+
+	for (IMouseListener* listener : mouse_listiners)
+		listener->OnMouseMoved(xoffset, yoffset);
 }
+
+std::vector<IKeyListener*> EventController::key_listiners;
+std::vector<IMouseListener*> EventController::mouse_listiners;
+bool EventController::key_press = false;
+bool EventController::key_hold = false;
+bool EventController::first_mouse_measurment = true;
+double EventController::last_y = 0;
+double EventController::last_x = 0;
--- simpleflight/EventController.h
+++ simpleflight/EventController.h
@@ -1,6 +1,8 @@
 #include <vector>
+#include <algorithm>
 #include "IMouseListener.h"
 #include "IKeyListener.h"
+#include "GLFW/glfw3.h"
 
 class EventController
 {
@@ -8,15 +10,20 @@ public:
 	EventController();
 	~EventController();
 	static EventController& GetInstance();
-	static void AddMouseListener(IMouseListener listener);
-	static void AddKeyListener(IKeyListener listener);
-	static void RemoveMouseListener(IMouseListener listener);
-	static void RemoveKeyListener(IKeyListener listener);
+	static void AddMouseListener(IMouseListener* listener);
+	static void AddKeyListener(IKeyListener* listener);
+	static void RemoveMouseListener(IMouseListener* listener);
+	static void RemoveKeyListener(IKeyListener* listener);
 	static void KeyNotify(int key, int action);
-	static void MouseNotify(int xpos, int ypos);
+	static void MouseNotify(double xpos, double ypos);
 
 private:
-	static std::vector<IKeyListener> key_listiners;
-	static std::vector<IMouseListener> mouse_listiners;
+	static std::vector<IKeyListener*> key_listiners;
+	static std::vector<IMouseListener*> mouse_listiners;
+	static bool key_press;
+	static bool key_hold;
+	static bool first_mouse_measurment;
+	static double last_y;
+	static double last_x;
 };
 
