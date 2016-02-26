e6bf2ccbe95b3eefe5d0ed8d6f1487f6699db741
jax9000 <jax1994@gmail.com>
2016-02-26 19:07:23 +0100

    Add Camera header

--- simpleflight/main.cpp
+++ simpleflight/main.cpp
@@ -16,18 +16,6 @@
 //Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 bool keys[1024];
 
-GLfloat deltaTime = 0.0f;
-GLfloat lastFrame = 0.0f;
-
-GLfloat yaw = -90.0f;	// Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right (due to how Eular angles work) so we initially rotate a bit to the left.
-GLfloat pitch = 0.0f;
-GLfloat lastX = WIDTH / 2.0;
-GLfloat lastY = HEIGHT / 2.0;
-
-
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 GLint GlewInit()
 {
